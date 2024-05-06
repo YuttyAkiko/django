b2720c38f192bf1217754fa8a15ba4b324302507
Yutty Akiko <yutty.akiko@gmail.com>
2024-05-06 16:01:04 -0300

    Criei os models, registrei os models no admim.py, fiz as migrações para criar as tabelas.

--- medico/models.py
+++ medico/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Criando os modelos
+class Especialidade(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    
+    def __str__(self):
+        return self.nome
+    
+class Medico(models.Model):
+    nome = models.CharField(max_length=200)
+    endereco = models.TextField()
+    telefone = models.CharField(max_length=30)
+    email = models.EmailField()
+    data_nascimento = models.DateField()
+    crm = models.CharField(max_length=30)
+    id_especialidade = models.ForeignKey("Especialidade", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
